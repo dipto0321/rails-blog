cebb1bc3b4a2d8035c3f2101287460eab8938f9c
dipto0321 <diptokmk47@gmail.com>
2018-11-21 09:56:01 +0600

    Add actions for
        - Create new token for safe url
        - Save this token in databse by encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :name,
@@ -14,8 +15,19 @@ class User < ApplicationRecord
   validates :password,
             :password_confirmation, presence: true, length: { minimum: 6 }
 
-  def self.digest(string)
+  class << self
+    def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
