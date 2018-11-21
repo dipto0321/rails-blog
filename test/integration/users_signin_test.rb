39f7d0ec8fd4959980c34e7f7d63ebcb244a733b
dipto0321 <diptokmk47@gmail.com>
2018-11-21 15:02:57 +0600

    Implement advane sign in

--- test/integration/users_signin_test.rb
+++ test/integration/users_signin_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersSigninTest < ActionDispatch::IntegrationTest
@@ -5,37 +7,51 @@ class UsersSigninTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "signin with invalid information" do
+  test 'signin with invalid information' do
     get signin_path
     assert_template 'sessions/new'
-    post signin_path, params: { session: { email:"", password: "" } }
+    post signin_path, params: { session: { email: '', password: '' } }
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
 
-  test "sign in with valid information followed by logout" do
+  test 'sign in with valid information followed by logout' do
     get signin_path
-    post signin_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
+    post signin_path, params: { session: { email: @user.email,
+                                           password: 'password' } }
     assert is_signed_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", signin_path, count: 0
-    assert_select "a[href=?]", signout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select 'a[href=?]', signin_path, count: 0
+    assert_select 'a[href=?]', signout_path
+    assert_select 'a[href=?]', user_path(@user)
     delete signout_path
     assert_not is_signed_in?
     assert_redirected_to root_url
     delete signout_path
     follow_redirect!
-    assert_select "a[href=?]", signin_path
-    assert_select "a[href=?]", signout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select 'a[href=?]', signin_path
+    assert_select 'a[href=?]', signout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 
-  test "authenticated? should return false for a user with nil digest" do
+  test 'authenticated? should return false for a user with nil digest' do
     assert_not @user.authenticated?('')
+  end
+
+  test 'login with remembering' do
+    sign_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    assert_not_empty cookies['remember_token']
+  end
+
+  test 'login without remembering' do
+    # Log in to set the cookie.
+    sign_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    sign_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
