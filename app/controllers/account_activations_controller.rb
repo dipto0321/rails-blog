cb0655d56c0b6596e3be707c043039048ee68c4e
dipto0321 <diptokmk47@gmail.com>
2018-11-23 18:22:28 +0600

    Findout "Invatid activation key" problem and fix it
        - For findout the problem I used debugger on
        AccountActivationsController
        - Then findout the problem was on authentication? user's action
        on returning false logic

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,20 @@
 class AccountActivationsController < ApplicationController
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
+    @user = User.find_by(email: params[:email])
+    # debugger
+    if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+      @user.activate
+      sign_in @user
       flash[:success] = "Account activated!"
-      redirect_to user
+      redirect_to @user
     else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
+  end
+
+  private
+
+  def user_email
   end
 end
