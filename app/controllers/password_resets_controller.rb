edc8b5f1c92d2c3b48fd9175c0abdd338def0418
dipto0321 <diptokmk47@gmail.com>
2018-11-26 10:32:59 +0600

    Ad column in user for reset and make 'create' action for reset_new

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,28 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: user_pwd_email_param)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
+  end
+
   def edit
+  end
+  def update
+    
+  end
+
+  private
+
+  def user_pwd_email_param
+    params[:password_reset][:email].downcase!
   end
 end
